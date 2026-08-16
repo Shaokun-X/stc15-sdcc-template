344c5cd9987cc6b5d9c6d06c37ed3de49391dbb1
Shaokun-X <shaokun.xie@outlook.fr>
2026-08-16 12:57:36 +0200

    improve

--- lib/config.h
+++ lib/config.h
@@ -5,12 +5,14 @@
 
 /*********************************************************/
 
-#define MAIN_Fosc		22118400L	//定义主时钟
-//#define MAIN_Fosc		12000000L	//定义主时钟
-//#define MAIN_Fosc		11059200L	//定义主时钟
-//#define MAIN_Fosc		 5529600L	//定义主时钟
-//#define MAIN_Fosc		24000000L	//定义主时钟
-
+// #define MAIN_Fosc		22118400L	//定义主时钟
+// #define MAIN_Fosc		12000000L	//定义主时钟
+// #define MAIN_Fosc		11059200L	//定义主时钟
+// #define MAIN_Fosc		 5529600L	//定义主时钟
+// #define MAIN_Fosc		24000000L	//定义主时钟
+#define MAIN_Fosc		32000000L	//定义主时钟
+#define STDIO_USART		USART1
+#define UART_QUEUE_MODE 1
 
 /*********************************************************/
 
